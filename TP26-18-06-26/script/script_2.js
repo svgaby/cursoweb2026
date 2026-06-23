6086eb3570d3d88666c2f57d4a7edafa5f1fd332
SvGaby <lordangeto2@gmail.com>
2026-06-23 16:55:43 -0300

    tp2602

--- TP26-18-06-26/script/script_2.js
+++ TP26-18-06-26/script/script_2.js
@@ -1,8 +1,96 @@
-console.log ("ping scrit2");
+console.log("ping scrit2");
 
 document
-.querySelector("#saludo2")
-.addEventListener("click", (e) => {
-console.log("HOLA  MUNDO ! !");
-console.log(e);
-});
+    .querySelector("#saludo2")
+    .addEventListener("click", (e) => {
+        console.log("HOLA  MUNDO ! !");
+        console.log(e);
+    });
+
+document
+    .querySelector("#eventosMouse")
+    .addEventListener("click", () => {
+        console.log("Evento Click");
+    });
+
+/*   document
+.querySelector("#eventosMouse")
+.addEventListener("mouseover", () =>{
+  console.log("Evento Click");
+})
+
+*/
+document
+    .querySelector("#eventosMouse")
+    .addEventListener("mouseout", () => {
+        console.log("Evento mouseout");
+    });
+
+document
+    .querySelector("#eventosMouse")
+    .addEventListener("mousedown", () => {
+        console.log("Evento mousedown");
+    });
+
+document
+    .querySelector("#eventosMouse")
+    .addEventListener("mouseup", () => {
+        console.log("Evento mouseup");
+    });
+
+document
+    .querySelector("#eventosMouse")
+    .addEventListener("whel", () => {
+        console.log("Evento whel");
+    });
+
+
+document
+    .querySelector("#eventosMouse")
+    .addEventListener("mousemove", (e) => {
+        // console.log("Evento mopusemove");
+        // console.log(e.offsetX + "/" + e.offsetY);
+        e.target.textContent = e.offsetX + "/" + e.offsetY    //document.querySelector("#eventosMouse")
+    });
+
+document
+    .querySelector("#eventosMouse")
+    .addEventListener("contextmenu", (e) => {
+        console.log("Evento contextmenu");
+        e.preventDefault();
+    });
+
+
+document
+    .querySelector("#eventoTeclado")
+    .addEventListener("keyup", () => {
+        console.log("Evento keyup");
+    });
+
+document
+    .querySelector("#eventoTeclado")
+    .addEventListener("keydown", () => {
+        console.log("Evento keydown");
+    });
+
+document
+    .querySelector("#eventoTeclado")
+    .addEventListener("keypress", (e) => {
+        console.log("Evento keypress");
+        console.log("tecla: " + e.key)
+        if (e.key === "a") { e.preventDefault() }
+    });
+
+document
+    .querySelector("#eventoTeclado")
+    .addEventListener("blur", (e) => {
+        console.log("Evento blur");
+        e.target.style.backgroundColor = "unset";
+    });
+
+document
+    .querySelector("#eventoTeclado")
+    .addEventListener("focus", (e) => {
+        console.log("Evento focus");
+        e.target.style.backgroundColor = "yellow";
+    });
